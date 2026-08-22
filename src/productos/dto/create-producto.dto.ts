803d1619663f337dabc25b834d8e7c5a8eafee6f
camilo-cdh <camilo.cdh.dev@gmail.com>
2026-08-22 18:01:19 -0400

    feat(productos): agrega DTO para crear productos

--- src/productos/dto/create-producto.dto.ts
+++ src/productos/dto/create-producto.dto.ts
@@ -1,1 +1,15 @@
-export class CreateProductoDto {}
+import { TipoConservacion } from '../enums/tipo-conservacion.enum';
+
+export class CreateProductoDto {
+  nombre: string;
+  descripcion: string;
+  ingredientes: string;
+  valorNutricional: string;
+  categoriaId: number;
+  proveedorId: number;
+  precio: number;
+  fechaVencimiento: Date;
+  descuento: number;
+  imagen: string;
+  tipoConservacion: TipoConservacion;
+}
