6ddd2da467d415c74d52c40dfcbfbec26fa6c66b
camilo-cdh <camilo.cdh.dev@gmail.com>
2026-08-23 22:22:39 -0400

    feat(categorias): crea y documenta endpoint para listar categorías

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe, DefaultValuePipe } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CategoriaResponseDto } from './dto/categoria-response.dto';
 
+@ApiTags('Categorias')
 @Controller('categorias')
 export class CategoriasController {
   constructor(private readonly categoriasService: CategoriasService) {}
 
-  @Post()
-  create(@Body() createCategoriaDto: CreateCategoriaDto) {
-    return this.categoriasService.create(createCategoriaDto);
-  }
-
+  @ApiOperation({
+    summary: 'Obtener categorías',
+    description: 'Obtiene una lista de categorías, opcionalmente filtradas por nombre.',
+  })
+  @ApiQuery({
+    name: 'nombre',
+    required: false,
+    type: String,
+    example: 'Mermelada',
+    description: 'Busca categorías cuyo nombre contenga el texto indicado',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de categorías obtenida correctamente.',
+    type: CategoriaResponseDto,
+    isArray: true,
+  })
   @Get()
-  findAll() {
-    return this.categoriasService.findAll();
+  findAll(
+    @Query('nombre') nombre?: string,
+  ): CategoriaResponseDto[] {
+    return [];
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriasService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoriaDto: UpdateCategoriaDto) {
-    return this.categoriasService.update(+id, updateCategoriaDto);
-  }
+  
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriasService.remove(+id);
-  }
 }
