e90a31bb262f0133b60668ded85c7a60058098b7
camilo-cdh <camilo.cdh.dev@gmail.com>
2026-08-23 22:09:14 -0400

    feat(categorias): crea entidad y DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
-  imports: [ProductosModule],
+  imports: [ProductosModule, CategoriasModule],
   controllers: [AppController],
   providers: [AppService],
 })
