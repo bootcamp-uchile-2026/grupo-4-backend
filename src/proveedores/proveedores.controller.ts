a7cf3a4bef44fb0ededaf0653f6469e642c0cf8c
camilo-cdh <camilo.cdh.dev@gmail.com>
2026-08-25 17:42:42 -0400

    feat(proveedores): documentar endpoint para obtener proveedor por ID

--- src/proveedores/proveedores.controller.ts
+++ src/proveedores/proveedores.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ProveedoresService } from './proveedores.service';
 import { CreateProveedorDto } from './dto/create-proveedor.dto';
 import { UpdateProveedorDto } from './dto/update-proveedor.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ProveedorResponseDto } from './dto/proveedor-response.dto';
 
 @ApiTags('Proveedores')
@@ -25,6 +25,42 @@ export class ProveedoresController {
   @Get()
   findAll(): ProveedorResponseDto[] {
     return [];
+  }
+
+
+  @ApiOperation({
+    summary: 'Obtener proveedor por ID',
+    description: 'Obtiene un proveedor específico mediante su identificador.',
+  })
+
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 1,
+    description: 'Identificador único del proveedor',
+  })
+
+  @ApiResponse({
+    status: 200,
+    description: 'Proveedor obtenido correctamente.',
+    type: ProveedorResponseDto,
+  })
+
+  @ApiResponse({
+    status: 400,
+    description: 'El ID del proveedor no es válido.',
+  })
+
+  @ApiResponse({
+    status: 404,
+    description: 'Proveedor no encontrado.',
+  })
+
+  @Get(':id')
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ): ProveedorResponseDto {
+    return {} as ProveedorResponseDto;
   }
 
 
