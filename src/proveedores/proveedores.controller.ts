62bbcc94aec50a61ef908c2f2248fc28971c8d29
camilo-cdh <camilo.cdh.dev@gmail.com>
2026-08-25 17:41:38 -0400

    feat(proveedores): documentar endpoint para listar proveedores

--- src/proveedores/proveedores.controller.ts
+++ src/proveedores/proveedores.controller.ts
@@ -2,11 +2,30 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProveedoresService } from './proveedores.service';
 import { CreateProveedorDto } from './dto/create-proveedor.dto';
 import { UpdateProveedorDto } from './dto/update-proveedor.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ProveedorResponseDto } from './dto/proveedor-response.dto';
 
+@ApiTags('Proveedores')
 @Controller('proveedores')
 export class ProveedoresController {
   constructor(private readonly proveedoresService: ProveedoresService) {}
 
+  @ApiOperation({
+    summary: 'Obtener proveedores',
+    description: 'Obtiene una lista de proveedores.',
+  })
+
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de proveedores obtenida correctamente.',
+    type: ProveedorResponseDto,
+    isArray: true,
+  })
+
+  @Get()
+  findAll(): ProveedorResponseDto[] {
+    return [];
+  }
 
 
 }
