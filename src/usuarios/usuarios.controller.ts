8ea12312c80651623f21776afd5ea8d9f90ecbad
camilo-cdh <camilo.cdh.dev@gmail.com>
2026-08-25 17:04:15 -0400

    feat(usuarios): agregar endpoint para obtener usuario por ID

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsuarioResponseDto } from './dto/usuarios-response.dto';
 
 @ApiTags('Usuarios')
@@ -26,5 +26,34 @@ export class UsuariosController {
   }
 
 
+  @ApiOperation({
+    summary: 'Obtener usuario por ID',
+    description: 'Obtiene un usuario específico mediante su identificador.',
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 1,
+    description: 'Identificador único del usuario',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Usuario obtenido correctamente.',
+    type: UsuarioResponseDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'El ID del usuario no es válido.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Usuario no encontrado.',
+  })
+  @Get(':id')
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ): UsuarioResponseDto {
+    return {} as UsuarioResponseDto;
+  }
 
 }
